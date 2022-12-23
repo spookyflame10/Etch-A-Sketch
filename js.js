f66f3b4c732e5f29b4a28337046f2f5b323b8fb4
Simon Lee <simonlee98853@gmail.com>
2022-12-22 20:40:36 -0500

    added new inputs such as rainbowPen. All working

--- js.js
+++ js.js
@@ -1,12 +1,33 @@
+//defaults
+const DEFAULT_SIZE =16;
+
+//changing
+let color ='black'; // default pen color
+let rainbowPen = false;
+
 const container = document.querySelector('.grid-container');
 const button = document.querySelector('button');
-const DEFAULTSIZE =16;
+const eraser = document.querySelector('button.eraser');
+const pen = document.querySelector('#pen-color');
+const clear = document.querySelector('#clear');
+const rainbow = document.querySelector('#rainbow');
 
+rainbow.onclick = () => {rainbowPen = true; return};
+clear.onclick = () => reloadGrid();
+pen.oninput = (e) => {rainbowPen = false; color = e.target.value;};
+eraser.onclick = () => {rainbowPen = false; color = 'white';};
 button.addEventListener('click', () => {
     let size = prompt("please enter size", 16);
     makeGrid(size);
 });
 
+function reloadGrid(){
+    clearGrid();
+    makeGrid(DEFAULT_SIZE);
+}
+function clearGrid(){
+    container.innerHTML = '';
+}
 function makeGrid(size){
     container.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -18,7 +39,10 @@ function makeGrid(size){
     }
 }
 function changeColor(e){
-    this.style.backgroundColor = getRandomColor();
+    if(rainbowPen)
+        this.style.backgroundColor = getRandomColor();
+    else
+        this.style.backgroundColor = color;
 }
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -29,5 +53,5 @@ function getRandomColor() {
     return color;
 }
 window.onload = () => {
-    makeGrid(DEFAULTSIZE)
+    makeGrid(DEFAULT_SIZE)
 }
